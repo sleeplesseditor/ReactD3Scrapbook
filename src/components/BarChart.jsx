30a9c5051fe34f283b510dd1bc2a74b4d514833d
sleeplesseditor <josh@bryson.me.uk>
2021-03-07 17:31:55 +0000

    BarChart display

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,23 +1,39 @@
 import * as React from 'react';
-import { csv } from 'd3';
+import { csv, scaleBand, scaleLinear, max } from 'd3';
 
 const csvUrl = 'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv';
+const width = 960;
+const height = 500;
 
 const BarChart = () => {
     const [data, setData] = React.useState(null);
 
     React.useEffect(() => {
-        csv(csvUrl).then(setData)
+        const row = d => {
+            d.Population = +d['2020'];
+            return d;
+        };
+        csv(csvUrl, row).then(data => {
+            setData(data.slice(0,10))
+        });
     }, []);
 
     if(!data) {
         return <pre>Loading...</pre>
     }
 
-    return (
-        <div>
+    const yScale = scaleBand()
+        .domain(data.map(d => d.Country))
+        .range([0, height]);
 
-        </div>
+    const xScale = scaleLinear()
+        .domain([0, max(data, d => d.Population)])
+        .range([0, width]);
+
+    return (
+        <svg width={width} height={height}>
+            {data.map(d => <rect x={0} y={yScale(d.Country)} width={xScale(d.Population)} height={yScale.bandwidth()} />)}
+        </svg>
     )
 };
 
