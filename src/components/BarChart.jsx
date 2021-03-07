5d98e591fedf8cfda00b4c96e3459b9ebedfc37c
sleeplesseditor <josh@bryson.me.uk>
2021-03-07 17:52:10 +0000

    Axes setup for Bar Chart

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -4,6 +4,12 @@ import { csv, scaleBand, scaleLinear, max } from 'd3';
 const csvUrl = 'https://gist.githubusercontent.com/curran/0ac4077c7fc6390f5dd33bf5c06cb5ff/raw/605c54080c7a93a417a3cea93fd52e7550e76500/UN_Population_2019.csv';
 const width = 960;
 const height = 500;
+const margin = {
+    top: 20,
+    right: 20,
+    bottom: 20,
+    left: 200
+}
 
 const BarChart = () => {
     const [data, setData] = React.useState(null);
@@ -22,17 +28,31 @@ const BarChart = () => {
         return <pre>Loading...</pre>
     }
 
+    const innerHeight = height - margin.top - margin.bottom;
+    const innerWidth = width - margin.left - margin.right;
+
     const yScale = scaleBand()
         .domain(data.map(d => d.Country))
-        .range([0, height]);
+        .range([0, innerHeight]);
 
     const xScale = scaleLinear()
         .domain([0, max(data, d => d.Population)])
-        .range([0, width]);
+        .range([0, innerWidth]);
 
     return (
         <svg width={width} height={height}>
-            {data.map(d => <rect x={0} y={yScale(d.Country)} width={xScale(d.Population)} height={yScale.bandwidth()} />)}
+            <g transform={`translate(${margin.left}, ${margin.top})`}>
+                {xScale.ticks().map(tickValue =>
+                    <g transform={`translate(${xScale(tickValue)}, 0)`}>
+                        <line y2={innerHeight} stroke="black" />
+                        <text dy=".71em" y={innerHeight + 3} style={{textAnchor: 'middle'}}>{tickValue}</text>
+                    </g>
+                )}
+                {yScale.domain().map(tickValue =>
+                    <text dy=".32em" x={-3} y={yScale(tickValue) + yScale.bandwidth() / 2} style={{textAnchor: 'end'}}>{tickValue}</text>
+                )}
+                {data.map(d => <rect x={0} y={yScale(d.Country)} width={xScale(d.Population)} height={yScale.bandwidth()} />)}
+            </g>
         </svg>
     )
 };
