0f011cadf81a962a0abcbb05fdfd5e4aac5712b4
sleeplesseditor <josh@bryson.me.uk>
2021-03-18 23:46:22 +0000

    Tooltip styling resovled

--- src/components/CityMap/CityMapHelpers.jsx
+++ src/components/CityMap/CityMapHelpers.jsx
@@ -15,9 +15,10 @@ export const Marks = ({
         const [a, b] = projection([tooltipState.lng, tooltipState.lat]);
         return (
             <g className="tooltip" x={a} y={b}>
-                <text x={a} y={b}>{tooltipState.city},</text>
-                <text x={a} y={b + 15}>{tooltipState.country}</text>
-                <text x={a} y={b + 30}>Pop.:  {tooltipState.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</text>
+                <rect x={a + 10} y={b - 15} width="100" height="50" className="tooltip-background"></rect>
+                <text x={a + 15} y={b}>{tooltipState.city},</text>
+                <text x={a + 15} y={b + 15}>{tooltipState.country}</text>
+                <text x={a + 15} y={b + 30}>Pop.:  {tooltipState.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</text>
             </g>
         )
     }
@@ -30,11 +31,11 @@ export const Marks = ({
                 <path key={feature.properties.name} className="land" d={path(feature)} />
             ))};
             <path className="interiors" d={path(interiors)} />
-            {tooltipState ? tooltipComponent() : null}
             {cities.map(d => {
                 const [x, y] = projection([d.lng, d.lat]);
                 return <circle className="city-marker" cx={x} cy={y} r={1.5} onMouseOver={() => setTooltipState(d)} onMouseOut={() => setTooltipState(null)} />
             })}
+            {tooltipState ? tooltipComponent() : null}
         </g>
     )
 }
