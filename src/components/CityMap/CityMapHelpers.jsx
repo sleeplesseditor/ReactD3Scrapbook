00a44cc4fa47f893fec573abea276e74ecb79f03
sleeplesseditor <josh@bryson.me.uk>
2021-03-18 23:23:35 +0000

    Text layout of tooltip revised

--- src/components/CityMap/CityMapHelpers.jsx
+++ src/components/CityMap/CityMapHelpers.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { geoGraticule, geoNaturalEarth1, geoPath } from 'd3';
+import { geoGraticule, geoNaturalEarth1, geoPath, select } from 'd3';
 
 const projection = geoNaturalEarth1();
 const path = geoPath(projection);
@@ -14,20 +14,20 @@ export const Marks = ({
     const tooltipComponent = () => {
         const [a, b] = projection([tooltipState.lng, tooltipState.lat]);
         return (
-            <g  x={a} y={b}>
-            <rect className="marker" x={a} y={b} />
-            <text className="marker-text" x={a} y={b}>{tooltipState.city}, {tooltipState.country}</text>
+            <g className="tooltip" x={a} y={b}>
+                <text x={a} y={b}>{tooltipState.city},</text>
+                <text x={a} y={b + 15}>{tooltipState.country}</text>
+                <text x={a} y={b + 30}>Pop.:  {tooltipState.population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</text>
             </g>
         )
     }
 
     return (
         <g className="marks">
-            {console.log('A', tooltipState)}
-            <path className="sphere" d={path({ type: 'Sphere' })} />
+            <path className="sphere" id="sphere" d={path({ type: 'Sphere' })} />
             <path className="graticules" d={path(graticule())} />
             {land.features.map(feature => (
-            <path key={feature.properties.name} className="land" d={path(feature)} />
+                <path key={feature.properties.name} className="land" d={path(feature)} />
             ))};
             <path className="interiors" d={path(interiors)} />
             {tooltipState ? tooltipComponent() : null}
