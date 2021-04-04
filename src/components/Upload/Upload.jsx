dc19bfc9f88b37d552315a91b9d694beacbdf311
sleeplesseditor <josh@bryson.me.uk>
2021-04-04 23:33:28 +0100

    Initial revision of CSV pie chart to re-usable component

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import * as d3 from 'd3';
 import CSVReader from 'react-csv-reader';
+import { CSVPieChart } from './UploadHelpers';
 import './upload.scss';
 
 function toCamel(string){
     return string.replace(/(?:_| |\b)(\w)/g, function($1){return $1.toUpperCase().replace('_',' ');});
 }
+
+const partyColours = ["#0087DC", "#DC241f", "#FDBB30", "#D46A4C", "#FFFF00", "#326760", "#008142", "##528D6B", "#CCC"];
 
 const Upload = () => {
     const [selectedFile, setSelectedFile] = React.useState(null);
@@ -50,97 +53,11 @@ const Upload = () => {
                 .text(function(d) { return d; });
     }
 
-
-const create_chart = (data) => {
-    const width = 860;
-    const height = 500;
-    const radius = Math.min(width, height) / 2;
-    const labelHeight = 18;
-  
-    const color = d3.scaleOrdinal()
-        .range(["#0087DC", "#DC241f", "#FDBB30", "#D46A4C", "#FFFF00", "#326760", "#008142", "##528D6B", "#CCC"]);
-  
-    const arc = d3.arc()
-        .outerRadius(radius - 80)
-        .innerRadius(0);
-  
-    const pie = d3.pie()
-        .sort(null)
-        .value(function(d) { return d.percentage_of_vote; });
-  
-    const svg = d3.select("#svg-container")
-        .append("svg")
-        .attr("width", width)
-        .attr("height", height)
-        .append("g")
-        .attr("transform", "translate(" + width / 3 + "," + height / 1.95 + ")");
-  
-    const g = svg.selectAll(".arc")
-        .data(pie(data))
-        .enter().append("g")
-        .attr("class", "arc");
-  
-    g.append("path")
-        .attr("d", arc)
-        .attr('stroke', 'black')
-        .attr("fill", function(d) {
-            return color(d.data.political_party);
-        })
-        .attr('id', function(d, i) {
-            return 'arc_' + i
-        })
-        .style('opacity', 0.7);
-  
-    const legend = svg
-        .append('g')
-        .attr('class', 'legend')
-        .attr('transform', `translate(${radius - 30}, -130)`);
-  
-    legend
-        .selectAll(null)
-        .data(pie(data))
-        .enter()
-        .append('rect')
-        .attr('y', (d, i) => labelHeight * i * 1.8)
-        .attr('width', labelHeight)
-        .attr('height', labelHeight)
-        .attr('fill', d => color(d.data.political_party))
-        .attr('stroke', 'grey')
-        .on('mouseover', function(d, i) {
-            d3.select('#arc_' + i.index).style("opacity", 1);
-            d3.select('tr.row' + i.index).style("background", "gray").style('color', 'white');
-        })
-        .on('mouseout', function(d, i) {
-            d3.selectAll("path").style("opacity", 0.7);
-            d3.select('tr.row' + i.index).style("background", "none").style('color', 'black');
-        })
-        .style('stroke-width', '1px')
-        .style('opacity', 0.7);
-
-    legend
-        .selectAll(null)
-        .data(pie(data))
-        .enter()
-        .append('text')
-        .text(d => d.data.political_party)
-        .attr('x', labelHeight * 1.5)
-        .attr('y', (d, i) => labelHeight * i * 1.79 + labelHeight)
-        .style('font-family', 'sans-serif')
-        .style('font-size', `${labelHeight}px`);
-    }
-
     React.useEffect(() => {
         if(isFilePicked) {
             create_table(selectedFile)
-            create_chart(selectedFile)
         }
     }, [isFilePicked, selectedFile]);
-
-    React.useEffect(() => {
-        if(selectedFile !== null) {
-            d3.select('#svg-container').select('svg').remove();
-        }
-    }, [selectedFile])
 
     return (
         <div className="main-container">
@@ -150,11 +67,18 @@ const create_chart = (data) => {
                 parserOptions={parsingOptions}
             />
             <p>Select a CSV file from the data folder</p>
-            {isFilePicked ? (
+            {selectedFile ? (
                 <div className="viz-container">
                     <div className="data-container">
                         <table id="table"></table>
-                        <div id="svg-container"></div>
+                        <CSVPieChart
+                            coloursArr={partyColours}
+                            data={selectedFile}
+                            height={500}
+                            id="svg-container"
+                            labelHeight={18}
+                            width={860}
+                        />
                     </div>
                     <h4>UK General Election {yearDisplay} Votes</h4>
                 </div>
---
+++ src/components/Upload/UploadHelpers.js
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import * as d3 from 'd3';
+
+export const CSVPieChart = ({ coloursArr, data, id, height, labelHeight, width }) => {
+    const radius = Math.min(width, height) / 2;
+  
+    const color = d3.scaleOrdinal()
+        .range(coloursArr);
+  
+    const arc = d3.arc()
+        .outerRadius(radius - 80)
+        .innerRadius(0);
+  
+    const pie = d3.pie()
+        .sort(null)
+        .value(function(d) { return d.percentage_of_vote; });
+
+    d3.select('#svg-container').select('svg').remove();
+  
+    const svg = d3.select("#svg-container")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 3 + "," + height / 1.95 + ")");
+  
+    const g = svg.selectAll(".arc")
+        .data(pie(data))
+        .enter().append("g")
+        .attr("class", "arc");
+  
+    g.append("path")
+        .attr("d", arc)
+        .attr('stroke', 'black')
+        .attr("fill", function(d) {
+            return color(d.data.political_party);
+        })
+        .attr('id', function(d, i) {
+            return 'arc_' + i
+        })
+        .style('opacity', 0.7);
+  
+    const legend = svg
+        .append('g')
+        .attr('class', 'legend')
+        .attr('transform', `translate(${radius - 30}, -130)`);
+  
+    legend
+        .selectAll(null)
+        .data(pie(data))
+        .enter()
+        .append('rect')
+        .attr('y', (d, i) => labelHeight * i * 1.8)
+        .attr('width', labelHeight)
+        .attr('height', labelHeight)
+        .attr('fill', d => color(d.data.political_party))
+        .attr('stroke', 'grey')
+        .on('mouseover', function(d, i) {
+            d3.select('#arc_' + i.index).style("opacity", 1);
+            d3.select('tr.row' + i.index).style("background", "gray").style('color', 'white');
+        })
+        .on('mouseout', function(d, i) {
+            d3.selectAll("path").style("opacity", 0.7);
+            d3.select('tr.row' + i.index).style("background", "none").style('color', 'black');
+        })
+        .style('stroke-width', '1px')
+        .style('opacity', 0.7);
+
+    legend
+        .selectAll(null)
+        .data(pie(data))
+        .enter()
+        .append('text')
+        .text(d => d.data.political_party)
+        .attr('x', labelHeight * 1.5)
+        .attr('y', (d, i) => labelHeight * i * 1.79 + labelHeight)
+        .style('font-family', 'sans-serif')
+        .style('font-size', `${labelHeight}px`);
+    
+    return (
+        <div id={id}>
+        </div>
+    )
+}
