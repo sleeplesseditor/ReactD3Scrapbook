a6999c320a4e026d516afa3efe97e7439aed4eaa
sleeplesseditor <josh@bryson.me.uk>
2021-04-25 17:25:46 +0100

    File tidying

--- src/components/RadialTree/RadialTreeHelpers.js
+++ src/components/RadialTree/RadialTreeHelpers.js
@@ -35,24 +35,3 @@ export {
     project,
     useData
 }
-
-// import * as React from 'react';
-// import { json } from 'd3';
-// import { feature, mesh } from 'topojson-client';
-
-// const jsonUrl = 'https://unpkg.com/world-atlas@2.0.2/countries-50m.json';
-
-// export const useWorldAtlas = () => {
-//     const [data, setData] = React.useState(null);
-
-//     React.useEffect(() => {
-//         json(jsonUrl).then(topology => {
-//             const { countries, land } = topology.objects;
-//             setData({
-//                 land: feature(topology, land),
-//                 interiors: mesh(topology, countries, (a, b) => a !== b)
-//             });
-//         });
-//     }, []);
-//     return data;
-// }
